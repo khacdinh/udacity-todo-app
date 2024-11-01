91d615621215429813650a30ac3d4e8b56105e86
Dinh Nguyen Khac <khacdinh09t4@gmail.com>
2024-11-01 20:29:34 +0700

    add udacity todo app

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+const apiId = 'jzf3h0b8v6';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`;
+
+export const authConfig = {
+  // Auth0 settings
+  domain: 'dev-ruf8wwfwb42bn8ls.us.auth0.com',
+  clientId: 'N9tw8ewXhiBEwIFXSnI5WqZWd5NI43y3',
+  callbackUrl: 'http://localhost:3000/callback'
+};
